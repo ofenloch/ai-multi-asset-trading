8b50db8e52ea62fc2c7df2d9b24b050e1f7b51bf
Oliver Ofenloch <dr.oliver.ofenloch@icloud.com>
2026-04-08 19:09:28 +0200

    save downloaded data to csv files
    disable yf auto adjustment to avoid different values between downloads

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -4,6 +4,7 @@ def load_data(tickers, start, end):
     data = {}
     for t in tickers:
         print(f"Loading data for {t} from {start} to {end} ...")
-        df = yf.download(t, start=start, end=end)
+        df = yf.download(t, start=start, end=end, auto_adjust=False)
+        df.to_csv(f"data/{t}.csv")  # Speichern als CSV
         data[t] = df
     return data
