a6a93c3d06aa85ad16b98154d2b8054f337db6d8
Oliver Ofenloch <dr.oliver.ofenloch@icloud.com>
2026-04-11 10:51:41 +0200

    use yf.history insted of downlod
    I like the resulting csv files better. Test is still OK.

--- utils/data_loader.py
+++ utils/data_loader.py
@@ -4,7 +4,8 @@ def load_data(tickers, start, end):
     data = {}
     for t in tickers:
         print(f"Loading data for {t} from {start} to {end} ...")
-        df = yf.download(t, start=start, end=end, auto_adjust=False)
+        #df = yf.download(t, start=start, end=end, auto_adjust=False)
+        df = yf.Ticker(t).history(interval='1d', start=start, end=end, auto_adjust=False)
         df.to_csv(f"data/{t}.csv")  # Speichern als CSV
         data[t] = df
     return data
